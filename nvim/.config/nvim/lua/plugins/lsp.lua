f6a92a5ee69ae400480a44ce31f43e7822e57e13
Aleksandar Rangelov <122702818+aleksandar-rangelov@users.noreply.github.com>
2024-01-03 08:09:17 +0200

    checkpoint

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -23,6 +23,7 @@ return {
             -- Autocompletion
             { 'hrsh7th/nvim-cmp' },     -- Required
             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lua' },
             { 'hrsh7th/cmp-buffer' },   -- Required
             { 'hrsh7th/cmp-path' },     -- Required
             { 'L3MON4D3/LuaSnip' },     -- Required
