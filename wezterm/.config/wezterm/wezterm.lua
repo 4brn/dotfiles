62d141676f7fc3c1e7d7dbbe3d5c2dc6479dcb0d
Aleksandar Rangelov <122702818+4brn@users.noreply.github.com>
2024-05-29 14:35:43 +0300

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,18 +2,17 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.front_end = "OpenGL"
-config.max_fps = 200
--- config.enable_wayland = false
--- config.animation_fps = 1
+config.max_fps = 60
+config.enable_wayland = true
 -- freetype_load_target = "Normal"
 config.dpi = 96
-config.font = wezterm.font("CommitMono Nerd Font")
-config.font_size = 16
-config.color_scheme = "GruvboxDark"
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 22
+config.color_scheme = "GitHub Dark"
 config.use_fancy_tab_bar = true
 config.window_background_opacity = 1.0
 config.default_cursor_style = "SteadyBlock"
--- config.cursor_blink_rate = 0
+config.cursor_blink_rate = 0
 
 config.window_padding = {
 	left = 0,
